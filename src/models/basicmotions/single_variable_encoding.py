eba0a69efb5ded1e6c1809dd24f7473650851e0a
Bhavesh Kalisetti <bhaveshkalisetti@Bhaveshs-MBP.attlocal.net>
2023-03-25 20:13:09 -0700

    Cleaning up data fns

--- src/models/basicmotions/single_variable_encoding.py
+++ src/models/basicmotions/single_variable_encoding.py
@@ -3,30 +3,10 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-from sktime.datasets import load_from_tsfile_to_dataframe
 
+from ...data.data import get_ds
 from ..single_variables import SiameseContrastiveLoss, EncodingModule, SingleVariableModulesWrapper
 from ...visualizations.umap_visualizer import UMAPLatent
-
-class BenchmarkDataset(torch.utils.data.Dataset):
-    def __init__(self, data):
-        self.data = data
-    def __len__(self):
-        return len(self.data)
-    def __getitem__(self, item):
-        return self.data[item][0], self.data[item][1]
-
-def get_ds(file, class_to_index):
-    df, y = load_from_tsfile_to_dataframe(file)
-    df = df.to_numpy()
-    dataset = []
-    for example in range(y.shape[0]):
-        stacked = np.stack([df[example][i].to_numpy() for i in range(df[example].shape[0])])
-        stacked = stacked.transpose()
-        dataset.append(tuple([stacked,class_to_index[y[example]]]))
-
-    return BenchmarkDataset(dataset)
-
 
 if __name__ == "__main__":
     class_to_index={"standing":0, "running":1, "walking":2,"badminton":3}
