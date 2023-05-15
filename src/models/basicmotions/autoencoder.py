270326fbb4035470530d5e7ce9dc87d4653b7e25
Bhavesh Kalisetti <bhaveshkalisetti@Bhaveshs-MBP.attlocal.net>
2023-05-14 20:24:08 -0700

    basicmotions decoding updates + autoencoding

---
+++ src/models/basicmotions/autoencoder.py
@@ -0,0 +1,143 @@
+import torch
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from ...data.data import get_ds
+from ..single_variables import SingleVariableModulesWrapper,EncodingModule, SiameseContrastiveLoss
+from ...visualizations.umap_visualizer import UMAPLatent
+
+class LSTMEncoder(torch.nn.Module):
+    def __init__(self, input_size, hidden):
+        super(LSTMEncoder, self).__init__()
+        self.input_size = input_size
+        self.hidden = hidden
+
+        self.lstm = torch.nn.LSTM(input_size=1, hidden_size=hidden, num_layers=1, batch_first=True)
+        self.lstm1 = torch.nn.LSTM(input_size=1, hidden_size=2*hidden, num_layers=1, batch_first=True)
+        self.lstm2 = torch.nn.LSTM(input_size=1, hidden_size=hidden, num_layers=1, batch_first=True)
+        self.linear = torch.nn.Linear(in_features=hidden, out_features=hidden)
+
+    def forward(self, x):
+        x, (hidden_n, cell_n) = self.lstm(x)
+        x = x[:, -1, :]
+        x = self.linear(x)
+        return x
+    
+class LSTMDecoder(torch.nn.Module):
+    def __init__(self, input_size, hidden):
+        super(LSTMDecoder, self).__init__()
+        self.input_size = input_size
+        self.hidden = hidden
+
+        self.lstm = torch.nn.LSTM(input_size=1, hidden_size=input_size, num_layers=1, batch_first=True)
+        self.lstm1 = torch.nn.LSTM(input_size=1, hidden_size=2*hidden, num_layers=1, batch_first=True)
+        self.lstm2 = torch.nn.LSTM(input_size=1, hidden_size=input_size, num_layers=1, batch_first=True)
+        self.linear = torch.nn.Linear(in_features=input_size, out_features=input_size)
+
+    def forward(self, x):
+        x = x.unsqueeze(2)
+        x, (hidden_n, cell_n) = self.lstm(x)
+        x = x[:, -1, :]
+        x = self.linear(x)
+        return x
+
+
+class SingleVariableAutoencoder(torch.nn.Module):
+    def __init__(self, input_size, hidden):
+        super(SingleVariableAutoencoder, self).__init__()
+        self.input_size = input_size
+        self.hidden = hidden
+
+        self.encoder = LSTMEncoder(input_size, hidden)
+        self.decoder = LSTMDecoder(input_size, hidden)
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded, encoded
+    
+class EncodingModule(torch.nn.Module):
+    def __init__(self, module_list):
+        super().__init__()
+        self.module_list = module_list
+        self.num_variables = len(module_list)
+    
+    def forward(self, data):
+        encoder_results = []
+        decoder_results = []
+        for i in range(self.num_variables):
+            data_subset = data[:, :, i]
+            decoded, encoded = self.module_list[i](data_subset.unsqueeze(2))
+            encoder_results.append(encoded)
+            decoder_results.append(decoded)
+        return decoder_results, encoder_results 
+
+if __name__ == "__main__":
+    class_to_index={"standing":0, "running":1, "walking":2,"badminton":3}
+    train_ds, test_ds = get_ds("data/basicmotions/BasicMotions_TRAIN.ts", class_to_index), get_ds("data/basicmotions/BasicMotions_TEST.ts", class_to_index)
+
+    autoencoders = []
+    for i in range(6):
+        autoencoders.append(SingleVariableAutoencoder(100, 10))
+    encoding_module = EncodingModule(torch.nn.ModuleList(autoencoders))
+
+    data_load = torch.utils.data.DataLoader(train_ds, len(train_ds), True)
+    opt = torch.optim.Adam(params=encoding_module.parameters(), lr=0.01)
+    sched = torch.optim.lr_scheduler.ExponentialLR(opt, 0.999)
+    contrastive_loss_fn = SiameseContrastiveLoss(m=1.0)
+    reconstruction_loss_fn = torch.nn.MSELoss()
+    batch_size = 40
+
+    epochs = 1000
+    for epoch in range(epochs):
+        for data_matrix, labels in data_load:
+            opt.zero_grad()
+            indices = torch.randperm(len(data_matrix))[:batch_size]
+            decoder_output, encoder_output = encoding_module(data_matrix[indices].float())
+            contrastive_loss = 0
+            reconstruction_loss = 0
+            for i in range(encoding_module.num_variables):
+                contrastive_loss += contrastive_loss_fn(encoder_output[i], labels[indices])
+                reconstruction_loss += reconstruction_loss_fn(decoder_output[i], data_matrix[indices][:, :, i].float())
+            total_loss = (1.0)*contrastive_loss + (0.)*reconstruction_loss
+            total_loss.backward()
+            opt.step()
+        sched.step()
+        print("Epoch: ", epoch, " Total Loss: ", float(total_loss),
+               "Contrastive Loss: ", float(contrastive_loss), "Reconstruction Loss: ", float(reconstruction_loss))
+
+    torch.save(encoding_module.state_dict(), "models/epilepsy/autoenc.dat")
+
+    encoding_module.load_state_dict(torch.load("models/epilepsy/autoenc.dat"))
+    data_train = torch.utils.data.DataLoader(train_ds, len(train_ds), True)
+    data_test = torch.utils.data.DataLoader(test_ds, len(test_ds), True)
+    with torch.no_grad():
+        for train_sample in data_test:
+            inp, out = train_sample[0].detach(), train_sample[1].detach()
+            for i in range(encoding_module.num_variables):
+                embeddings = encoding_module.module_list[i].encoder(inp[:,:,i].unsqueeze(2).float())
+                visualizer = UMAPLatent()
+                visualizer.visualize(embeddings, out, len(class_to_index))
+    plt.show()
+
+    encoding_module.load_state_dict(torch.load("models/epilepsy/autoenc.dat"))
+
+    with torch.no_grad():
+        for data_matrix, labels in data_test:
+            index = random.randint(0, len(data_matrix) - 1)
+            print(labels[index])
+            orig = data_matrix[index]
+            
+            decoded_output, _ = encoding_module(data_matrix.float())
+
+            fig, axes = plt.subplots(1, 6, figsize=(12,4))
+            for i in range(6):
+                ax = axes[i]
+                decoded_point = decoded_output[i][index]
+                ax.plot(orig[:, i], c='b', lw=0.5)
+                ax.plot(decoded_point, c='r', lw=0.5)
+
+            plt.show()
+
+
+
--- src/models/basicmotions/single_variable_decoding.py
+++ src/models/basicmotions/single_variable_decoding.py
@@ -1,5 +1,6 @@
 import torch
-
+import random
+import matplotlib.pyplot as plt
 from ...data.data import get_ds
 from ..single_variables import EncodingModule, DecodingModule, SingleVariableModulesWrapper
 
@@ -17,18 +18,20 @@ if __name__ == "__main__":
     opt = torch.optim.Adam(params=decoding_module.parameters(), lr=0.1)
     sched = torch.optim.lr_scheduler.ExponentialLR(opt, 0.999)
     loss = torch.nn.MSELoss()
+    batch_size = 40
 
-    epochs = 2000
+    epochs = 100
     for epoch in range(epochs):
         for data_matrix, labels in data_load:
+            indices = torch.randperm(len(data_matrix))[:batch_size]
             with torch.no_grad():
-                encoded = encoding_module(data_matrix.float())
+                encoded = encoding_module(data_matrix[indices].float())
 
             decoded = decoding_module(encoded)
 
             total_loss = 0
             for i in range(decoding_module.num_variables):
-                total_loss += loss(decoded[:, :, i].float(), data_matrix[:, :, i].float())
+                total_loss += loss(decoded[:, :, i].float(), data_matrix[indices][:, :, i].float())
             opt.zero_grad()
             total_loss.backward()
             opt.step()
@@ -36,3 +39,25 @@ if __name__ == "__main__":
         print("Epoch: ", epoch, " Total Loss: ", float(total_loss))
 
     torch.save(decoding_module.state_dict(), "models/basicmotions/dec.dat")
+
+    decoding_module.load_state_dict(torch.load("models/basicmotions/dec.dat"))
+
+    data_test = torch.utils.data.DataLoader(test_ds, len(test_ds), True)
+    with torch.no_grad():
+        for data_matrix, labels in data_load:
+            index = random.randint(0, len(data_matrix) - 1)
+            orig = data_matrix[index]
+            
+            encoded = encoding_module(data_matrix.float())
+            decoded = decoding_module(encoded)[index]
+
+            fig, axes = plt.subplots(2, 3)
+            for i in range(2):
+                for j in range(3):
+                    ax = axes[i][j]
+                    decoded_point = decoded[:, 3*i + j]
+                    ax.plot(orig[:, 3*i + j], c='b', lw=0.5)
+                    ax.plot(decoded_point, c='r', lw=0.5)
+
+            plt.show()
+
