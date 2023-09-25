043caf873c86b5a4a96893727656f93ef3ea0990
Bhavesh Kalisetti <bhaveshkalisetti@Bhaveshs-MacBook-Pro.local>
2023-09-24 22:26:05 -0700

    generalized further

--- src/experiment.py
+++ src/experiment.py
@@ -8,34 +8,57 @@ from src.utils.utils import *
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def intra_cluster_distance(wrapper, dl):
+def intra_cluster_distance(wrapper, dl, dataset):
     class_to_pattern = get_class_to_pattern_map().to(device)
     wrapper.eval()
     with torch.no_grad():
         for data_matrix, labels in dl:
             data_matrix, labels = data_matrix.to(device), labels.to(device)
-            for var in range(wrapper.num_variables - 1):
-                patterns = class_to_pattern[labels, var].to(device)
-                encoder = wrapper.single_variable_prototype_modules[var].encoder   
-                single_variable_data = data_matrix[:, :, var].unsqueeze(2).float()
-                embeddings = encoder(single_variable_data)
+            if dataset.startswith("simulated"):
+                for var in range(wrapper.num_variables - 1):
+                    patterns = class_to_pattern[labels, var].to(device)
+                    encoder = wrapper.single_variable_prototype_modules[var].encoder   
+                    single_variable_data = data_matrix[:, :, var].unsqueeze(2).float()
+                    embeddings = encoder(single_variable_data)
 
-                unique_patterns = torch.unique(patterns)
-                pattern_to_distance = {}
+                    unique_patterns = torch.unique(patterns)
+                    pattern_to_distance = {}
 
-                for pattern in unique_patterns:
-                    mask = patterns == pattern
-                    cluster_points = embeddings[mask]
-                    expanded_cluster_points_1 = cluster_points.unsqueeze(1)
-                    expanded_cluster_points_2 = cluster_points.unsqueeze(0)
-                    
-                    distances = torch.norm(expanded_cluster_points_1 - expanded_cluster_points_2, dim=2)
+                    for pattern in unique_patterns:
+                        mask = patterns == pattern
+                        cluster_points = embeddings[mask]
+                        expanded_cluster_points_1 = cluster_points.unsqueeze(1)
+                        expanded_cluster_points_2 = cluster_points.unsqueeze(0)
+                        
+                        distances = torch.norm(expanded_cluster_points_1 - expanded_cluster_points_2, dim=2)
 
-                    avg_distance = torch.mean(distances.tril(diagonal=-1))
+                        avg_distance = torch.mean(distances.tril(diagonal=-1))
 
-                    pattern_to_distance[pattern.item()] = avg_distance.item()
-                print("variable: " + str(var))
-                print(pattern_to_distance)
+                        pattern_to_distance[pattern.item()] = avg_distance.item()
+                    print("variable: " + str(var))
+                    print(pattern_to_distance)
+            else:
+                for var in range(wrapper.num_variables):
+                    encoder = wrapper.single_variable_prototype_modules[var].encoder   
+                    single_variable_data = data_matrix[:, :, var].unsqueeze(2).float()
+                    embeddings = encoder(single_variable_data)
+
+                    unique_labels = torch.unique(labels)
+                    labels_to_distance = {}
+
+                    for label in unique_labels:
+                        mask = labels == label
+                        cluster_points = embeddings[mask]
+                        expanded_cluster_points_1 = cluster_points.unsqueeze(1)
+                        expanded_cluster_points_2 = cluster_points.unsqueeze(0)
+                        
+                        distances = torch.norm(expanded_cluster_points_1 - expanded_cluster_points_2, dim=2)
+
+                        avg_distance = torch.mean(distances.tril(diagonal=-1))
+
+                        labels_to_distance[label.item()] = avg_distance.item()
+                    print("variable: " + str(var))
+                    print(labels_to_distance)
 
 def main(args):
     dataset = args.dataset
@@ -48,7 +71,7 @@ def main(args):
     wrapper = load_single_variable_prototypes_wrapper(config).to(device)
 
     if args.type == "intra":
-        intra_cluster_distance(wrapper, test_dataloader)
+        intra_cluster_distance(wrapper, test_dataloader, dataset)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
