f62e725cee28f68964b85d0d0becc1dd988b898e
Bhavesh Kalisetti <bhaveshkalisetti@Bhaveshs-MBP.attlocal.net>
2023-11-20 22:37:18 -0800

    sv vis for all but simulated

--- src/visualizations/paper.py
+++ src/visualizations/paper.py
@@ -1,3 +1,5 @@
+import argparse
+
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -34,9 +36,9 @@ charactertrajectories_filtered_encoders = load_encoders(charactertrajectories_fi
 charactertrajectories_filtered_sv_prototype_modules = load_single_variable_prototypes_wrapper(charactertrajectories_filtered_config)
 charactertrajectories_filtered_mv_module = load_multivariable_prototypes(charactertrajectories_filtered_config)
 
-simulated_config = get_config_from_dataset("simulated")
-simulated_train_ds = get_ds(get_train_path_from_dataset("simulated"), simulated_config['class_to_index'])
-simulated_test_ds = get_ds(get_test_path_from_dataset("simulated"), simulated_config['class_to_index'])
+simulated_config = get_config_from_dataset("simulated_6400")
+simulated_train_ds = get_ds(get_train_path_from_dataset("simulated_6400"), simulated_config['class_to_index'])
+simulated_test_ds = get_ds(get_test_path_from_dataset("simulated_6400"), simulated_config['class_to_index'])
 simulated_train_dl = torch.utils.data.DataLoader(simulated_train_ds, len(simulated_train_ds), shuffle=True)
 simulated_test_dl = torch.utils.data.DataLoader(simulated_test_ds, len(simulated_test_ds), shuffle=True)
 simulated_encoders = load_encoders(simulated_config)
@@ -44,6 +46,9 @@ simulated_sv_prototype_modules = load_single_variable_prototypes_wrapper(simulat
 simulated_mv_module = load_multivariable_prototypes(simulated_config)
 
 tab10 = plt.cm.get_cmap("tab10")
+all_colors = list(tab10.colors)
+
+plt.rcParams['font.family'] = 'Arial'
 
 def simulated_dataset_generation(save=False):
     return
@@ -67,9 +72,10 @@ def simulated_one_stage(save=False):
     return
 
 def epilepsy_single_variable_prototypes(save=False):
-    with torch.no_grad(save=False):
+    with torch.no_grad():
         fig, axs = plt.subplots(1, 3, figsize=(12, 4), sharex=True, sharey=True)
         variable_names = ["Acc (x)", "Acc (y)", "Acc (z)"]
+        colors = all_colors[:4] + [all_colors[6]]
         for j in range(3):
             ax = axs[j]
             ax.set_title(variable_names[j])
@@ -85,18 +91,27 @@ def epilepsy_single_variable_prototypes(save=False):
                 labels_with_protos = torch.concat([labels, epilepsy_config["num_classes"]*torch.ones((sv_module.prototypes.shape[0],))], dim=0)
 
                 reducer = umap.UMAP()
-                embeddings_2d = reducer.fit_transform(embeddings)
+                embeddings_2d = reducer.fit_transform(embeddings_with_protos)
                 e_min, e_max = np.min(embeddings_2d, 0), np.max(embeddings_2d, 0)
                 embeddings_2d = (embeddings_2d - e_min) / (e_max - e_min)
 
-                for k, label in enumerate(epilepsy_config["classes"]):
-                    idx = np.where(labels == label)[0]
-                    ax.scatter(embeddings_2d[idx, 0], embeddings_2d[idx, 1], label=label, c=colors[k], s=50, alpha=0.5)
+                for k in range(4):
+                    idx = np.where(labels_with_protos == k)[0]
+                    ax.scatter(embeddings_2d[idx, 0], embeddings_2d[idx, 1], label=k, color=colors[k], s=50, alpha=0.5)
 
+                idx = np.where(labels_with_protos == 4)[0]
+                ax.scatter(embeddings_2d[idx, 0], embeddings_2d[idx, 1], label=4, marker="*", edgecolor='black', linewidth=1.5, s=120, c=colors[-1])
 
+        legend_names = epilepsy_config['classes'] + ["Prototype"]
+        handles = [plt.Line2D([0], [0], marker='o' if c != 4 else '*', color='w', label=legend_names[c],
+                       markersize=10 if c != 4 else 11, markerfacecolor=colors[c], markeredgecolor='None' if c != 4 else 'black') for c in range(5)]
+        fig.legend(handles=handles, ncol=5, loc='lower center')
+        plt.tight_layout()
+        fig.subplots_adjust(bottom=0.1)
 
-
-    return
+        if save:
+            plt.savefig("visualizations/paper/epilepsy_sv.pdf", dpi=300)
+        plt.show()
 
 def epilepsy_multivariable_prototypes(save=False):
     return
@@ -105,7 +120,46 @@ def epilepsy_projections(save=False):
     return
 
 def charactertrajectories_filtered_single_variable_prototypes(save=False):
-    return
+    with torch.no_grad():
+        fig, axs = plt.subplots(1, 3, figsize=(12, 4), sharex=True, sharey=True)
+        variable_names = ["x", "y", "Pen Tip Force"]
+        colors = all_colors[:4] + [all_colors[6]]
+        for j in range(3):
+            ax = axs[j]
+            ax.set_title(variable_names[j])
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+            encoder = charactertrajectories_filtered_encoders[j]
+            sv_module = charactertrajectories_filtered_sv_prototype_modules.single_variable_prototype_modules[j]
+            for data_matrix, labels in charactertrajectories_filtered_test_dl:
+                single_variable_data = data_matrix[:, :, j].unsqueeze(2).float()
+                embeddings = encoder(single_variable_data)
+                embeddings_with_protos = torch.concat([embeddings, sv_module.prototypes], dim=0)
+                labels_with_protos = torch.concat([labels, charactertrajectories_filtered_config["num_classes"]*torch.ones((sv_module.prototypes.shape[0],))], dim=0)
+
+                reducer = umap.UMAP()
+                embeddings_2d = reducer.fit_transform(embeddings_with_protos)
+                e_min, e_max = np.min(embeddings_2d, 0), np.max(embeddings_2d, 0)
+                embeddings_2d = (embeddings_2d - e_min) / (e_max - e_min)
+
+                for k in range(4):
+                    idx = np.where(labels_with_protos == k)[0]
+                    ax.scatter(embeddings_2d[idx, 0], embeddings_2d[idx, 1], label=k, color=colors[k], s=50, alpha=0.5)
+
+                idx = np.where(labels_with_protos == 4)[0]
+                ax.scatter(embeddings_2d[idx, 0], embeddings_2d[idx, 1], label=4, marker="*", edgecolor='black', linewidth=1.5, s=120, c=colors[-1])
+
+        legend_names = charactertrajectories_filtered_config['classes'] + ["Prototype"]
+        handles = [plt.Line2D([0], [0], marker='o' if c != 4 else '*', color='w', label=legend_names[c],
+                       markersize=10 if c != 4 else 11, markerfacecolor=colors[c], markeredgecolor='None' if c != 4 else 'black') for c in range(5)]
+        fig.legend(handles=handles, ncol=5, loc='lower center')
+        plt.tight_layout()
+        fig.subplots_adjust(bottom=0.1)
+
+        if save:
+            plt.savefig("visualizations/paper/charactertrajectories_filtered_sv.pdf", dpi=300)
+        plt.show()
 
 def charactertrajectories_filtered_multivariable_prototypes(save=False):
     return
@@ -121,10 +175,57 @@ def charactertrajectories_filtered_no_contrastive_single_variable_prototypes(sav
     return
 
 def basicmotions_single_variable_prototypes(save=False):
-    return
+    with torch.no_grad():
+        fig, axs = plt.subplots(2, 3, figsize=(12, 8), sharex=True, sharey=True)
+        variable_names = ["Acc (x)", "Acc (y)", "Acc (z)", "Gyro (x)", "Gyro (y)", "Gyro (z)"]
+        colors = all_colors[:4] + [all_colors[6]]
+        for i in range(2):
+            for j in range(3):
+                ax = axs[i][j]
+                ax.set_title(variable_names[3*i + j])
+                ax.set_xticks([])
+                ax.set_yticks([])
+
+                encoder = basicmotions_encoders[j]
+                sv_module = basicmotions_sv_prototype_modules.single_variable_prototype_modules[j]
+                for data_matrix, labels in basicmotions_test_dl:
+                    single_variable_data = data_matrix[:, :, j].unsqueeze(2).float()
+                    embeddings = encoder(single_variable_data)
+                    embeddings_with_protos = torch.concat([embeddings, sv_module.prototypes], dim=0)
+                    labels_with_protos = torch.concat([labels, basicmotions_config["num_classes"]*torch.ones((sv_module.prototypes.shape[0],))], dim=0)
+
+                    reducer = umap.UMAP()
+                    embeddings_2d = reducer.fit_transform(embeddings_with_protos)
+                    e_min, e_max = np.min(embeddings_2d, 0), np.max(embeddings_2d, 0)
+                    embeddings_2d = (embeddings_2d - e_min) / (e_max - e_min)
+
+                    for k in range(4):
+                        idx = np.where(labels_with_protos == k)[0]
+                        ax.scatter(embeddings_2d[idx, 0], embeddings_2d[idx, 1], label=k, color=colors[k], s=50, alpha=0.5)
+
+                    idx = np.where(labels_with_protos == 4)[0]
+                    ax.scatter(embeddings_2d[idx, 0], embeddings_2d[idx, 1], label=4, marker="*", edgecolor='black', linewidth=1.5, s=120, c=colors[-1])
+
+        legend_names = basicmotions_config['classes'] + ["Prototype"]
+        handles = [plt.Line2D([0], [0], marker='o' if c != 4 else '*', color='w', label=legend_names[c],
+                       markersize=10 if c != 4 else 11, markerfacecolor=colors[c], markeredgecolor='None' if c != 4 else 'black') for c in range(5)]
+        fig.legend(handles=handles, ncol=5, loc='lower center')
+        plt.tight_layout()
+        fig.subplots_adjust(bottom=0.05)
+
+        if save:
+            plt.savefig("visualizations/paper/basicmotions_sv.pdf", dpi=300)
+        plt.show()
 
 def basicmotions_multivariable_prototypes(save=False):
     return
 
 def basicmotions_projection(save=False):
     return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save", action=argparse.BooleanOptionalAction, default=False, help="Whether to save the figure or not")
+    args = parser.parse_args()
+
+    basicmotions_single_variable_prototypes(save=args.save)
