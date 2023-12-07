efca783dbb63fc2c3222dd09e2cfca314d08c1d3
Bhavesh Kalisetti <bhaveshkalisetti@Bhaveshs-MBP.attlocal.net>
2023-12-07 00:08:47 -0800

    full ct data/config changes

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -52,34 +52,65 @@ def main():
     #     f.writelines(data)
 
 
-    mapping = {":2": ":b", ":4": ":d", ":12": ":p", ":13": ":q"}
-    with open("data/charactertrajectories/raw/CharacterTrajectories_TRAIN.ts", 'r') as infile:
-        lines = infile.readlines()
+    # mapping = {":2": ":b", ":4": ":d", ":12": ":p", ":13": ":q"}
+    # with open("data/charactertrajectories/raw/CharacterTrajectories_TRAIN.ts", 'r') as infile:
+    #     lines = infile.readlines()
 
-    with open("data/charactertrajectories_filtered/raw/train.ts", 'w') as outfile:
+    # with open("data/charactertrajectories_filtered/raw/train.ts", 'w') as outfile:
+    #     for i, line in enumerate(lines):
+    #         if i < 42:
+    #             outfile.write(line)
+    #         else:
+    #             stripped_line = line.strip()
+    #             for old, new in mapping.items():
+    #                 if stripped_line.endswith(old):
+    #                     outfile.write(stripped_line[:-len(old)] + new + "\n")
+    #                     break
+
+    # with open("data/charactertrajectories/raw/CharacterTrajectories_TEST.ts", 'r') as infile:
+    #     lines = infile.readlines()
+
+    # with open("data/charactertrajectories_filtered/raw/test.ts", 'w') as outfile:
+    #     for i, line in enumerate(lines):
+    #         if i < 42:
+    #             outfile.write(line)
+    #         else:
+    #             stripped_line = line.strip()
+    #             for old, new in mapping.items():
+    #                 if stripped_line.endswith(old):
+    #                     outfile.write(stripped_line[:-len(old)] + new + "\n")
+    #                     break
+
+    with open("data/charactertrajectories/processed/train.ts", 'r') as infile:
+        lines = infile.readlines()
+    
+    with open("data/charactertrajectories/processed/train_new.ts", 'w') as outfile:
         for i, line in enumerate(lines):
-            if i < 42:
+            if i < 7:
                 outfile.write(line)
             else:
-                stripped_line = line.strip()
-                for old, new in mapping.items():
-                    if stripped_line.endswith(old):
-                        outfile.write(stripped_line[:-len(old)] + new + "\n")
-                        break
+                line = line.strip()
+                data = "".join(line.split(":")[:-1])
+                label = int(line.split(":")[-1])
+                new_label = label - 1
+                new_line = data + str(new_label)
+                outfile.write(new_line)
 
-    with open("data/charactertrajectories/raw/CharacterTrajectories_TEST.ts", 'r') as infile:
+    with open("data/charactertrajectories/processed/test.ts", 'r') as infile:
         lines = infile.readlines()
-
-    with open("data/charactertrajectories_filtered/raw/test.ts", 'w') as outfile:
+    
+    with open("data/charactertrajectories/processed/test_new.ts", 'w') as outfile:
         for i, line in enumerate(lines):
-            if i < 42:
+            if i < 7:
                 outfile.write(line)
             else:
-                stripped_line = line.strip()
-                for old, new in mapping.items():
-                    if stripped_line.endswith(old):
-                        outfile.write(stripped_line[:-len(old)] + new + "\n")
-                        break
+                line = line.strip()
+                data = "".join(line.split(":")[:-1])
+                label = int(line.split(":")[-1])
+                new_label = label - 1
+                new_line = data + str(new_label)
+                outfile.write(new_line)
+
 
 if __name__ == "__main__":
     main()
